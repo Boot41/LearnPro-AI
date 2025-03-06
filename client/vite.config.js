341636814f45ff84860b664ee892e3111cffab34
aryan-think <aryan.kamboj@think41@gmail.com>
2025-03-06 14:32:13 +0530

    vite configs changed to use standard file paths when in dev mode

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,12 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
-export default defineConfig({
-  base:'/static/',
-  plugins: [react(),tailwindcss()],
+export default defineConfig(
+  ({ command }) => {
+  // Use root path in development and /static/ in production
+  const basePath = command === 'serve' ? '/' : '/static/'
+  return {
+    plugins: [react(),tailwindcss()],
+    base: basePath,
+  }
 })
